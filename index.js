91ba54d0c95e09261aba707ed9d46007926fa707
Walter Oro Abrahao <orowalter2000@gmail.com>
2020-06-21 03:11:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello, everybody";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "`Hello, ${specialGuest}!`" + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = "Let's talk about ${topic}.";
+var conversation = `${topic}`;
